4fa7fa3b388767a5704cb8ed5edf77cf0bcbab9c
Alan M <17537489+Am3ra@users.noreply.github.com>
2021-03-09 19:14:16 -0600

    add footer

--- App.js
+++ App.js
@@ -1,16 +1,41 @@
-var fs = require('fs');
-const http = require('http');
-
-const hostname = '127.0.0.1';
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+const hostname = "127.0.0.1";
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' });
-  fs.createReadStream('index.html').pipe(res);
-  // console.log(req);
+const server = http.createServer((req, response) => {
+  // var path = url.parse(request.url).pathname;
+  console.log(req.url);
+  if (/^\/[a-zA-Z0-9\/]*.css$/.test(request.url.toString())) {
+    sendFileContent(response, request.url.toString().substring(1), "text/css");
+  } else if (request.url === "/index") {
+    fs.readFile("index.html", function (err, data) {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(data);
+      response.end();
+    });
+  }
 });
+
+function sendFileContent(response, fileName, contentType) {
+  fs.readFile(fileName, function (err, data) {
+    if (err) {
+      response.writeHead(404);
+      response.write("Not Found!");
+    } else {
+      response.writeHead(200, { "Content-Type": contentType });
+      response.write(data);
+    }
+    response.end();
+  });
+}
+// fs.readFile("index.html", function (err, data) {
+//     response.writeHead(200, { "Content-Type": "text/html" });
+//     response.write(data);
+//     response.end();
+//   });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
- 
