77e40d92034961d312194444bb1aa50617b9281f
am3ra <jp2kk2@gmail.com>
2021-04-20 23:38:28 -0500

    Code formatting

--- app/src/components/general/Footer.vue
+++ app/src/components/general/Footer.vue
@@ -1,47 +1,47 @@
 <template>
   <q-footer class="Footer">
-      <q-toolbar>
-        <q-toolbar-title class="Footer__copyright">
-          © la lele 2021
-        </q-toolbar-title>
-        <q-btn
-          flat
-          round
-          class="Footer__social-link"
-          color="white"
-          icon="fab fa-instagram"
-          type="a" 
-          href="https://www.instagram.com/lelemoonnart/" 
-          target="_blank"
-        />
-        <q-btn
-          flat
-          round
-          class="Footer__social-link"
-          color="white"
-          icon="fab fa-twitter"
-          type="a" 
-          href="https://twitter.com/Lelemoonn" 
-          target="_blank"
-        />
-		 <q-btn
-          flat
-          round
-          class="Footer__social-link"
-          color="white"
-          icon="fab fa-twitch"
-          type="a" 
-          href="twitch.tv/lelemoonn" 
-          target="_blank"
-        />
-      </q-toolbar>
-    </q-footer>
+    <q-toolbar>
+      <q-toolbar-title class="Footer__copyright">
+        © la lele 2021
+      </q-toolbar-title>
+      <q-btn
+        flat
+        round
+        class="Footer__social-link"
+        color="white"
+        icon="fab fa-instagram"
+        type="a"
+        href="https://www.instagram.com/lelemoonnart/"
+        target="_blank"
+      />
+      <q-btn
+        flat
+        round
+        class="Footer__social-link"
+        color="white"
+        icon="fab fa-twitter"
+        type="a"
+        href="https://twitter.com/Lelemoonn"
+        target="_blank"
+      />
+      <q-btn
+        flat
+        round
+        class="Footer__social-link"
+        color="white"
+        icon="fab fa-twitch"
+        type="a"
+        href="twitch.tv/lelemoonn"
+        target="_blank"
+      />
+    </q-toolbar>
+  </q-footer>
 </template>
 
 <script>
 export default {
-  name: 'Footer'
-}
+  name: "Footer",
+};
 </script>
 
 <style lang="scss">
