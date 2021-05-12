8d2d995ba61072ae3e7dcb810b3217e599411f9b
iangranados <ian.live1@gmail.com>
2021-05-11 20:06:52 -0500

    Clientside for types

--- app/src/pages/ComisionesMain.vue
+++ app/src/pages/ComisionesMain.vue
@@ -22,63 +22,25 @@
         commission.
       </p>
     </div>
-    <q-separator color="light-gray" />
-    <div class="Comisiones__section">
-      <div class="Comisiones__Category row q-col-gutter-lg">
-        <div class="Category__description col-xs-12 col-md-7">
-          <h2 class="SectionTitle">Twitch Emotes / FB Stickers</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste natus
-            fugiat itaque? Excepturi laborum saepe consequatur iste recusandae
-            aspernatur molestias sit ex itaque
-          </p>
-          <ul>
-            <li>Price: $$$</li>
-            <li>Includes png, psd, jpg</li>
-          </ul>
-        </div>
-        <div class="Category__placeholder col-xs-12 col-md-5">
-          <q-img src="~assets/images/placeholder_thumb.png" alt="placeholder" />
-        </div>
-      </div>
+    <div v-if="!!loading && tipos.length <= 0" class="text-center">
+      <q-circular-progress indeterminate size="40px" color="primary" />
     </div>
-    <q-separator color="light-gray" />
-    <div class="Comisiones__section">
-      <div class="Comisiones__Category row q-col-gutter-lg">
-        <div class="Category__description col-xs-12 col-md-7">
-          <h2 class="SectionTitle">Twitch Badges</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste natus
-            fugiat itaque? Excepturi laborum saepe consequatur iste recusandae
-            aspernatur molestias sit ex itaque
-          </p>
-          <ul>
-            <li>Price: $$$</li>
-            <li>Includes png, psd, jpg</li>
-          </ul>
-        </div>
-        <div class="Category__placeholder col-xs-12 col-md-5">
-          <q-img src="~assets/images/placeholder_thumb.png" alt="placeholder" />
-        </div>
-      </div>
-    </div>
-    <q-separator color="light-gray" />
-    <div class="Comisiones__section">
-      <div class="Comisiones__Category row q-col-gutter-lg">
-        <div class="Category__description col-xs-12 col-md-7">
-          <h2 class="SectionTitle">Full body</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste natus
-            fugiat itaque? Excepturi laborum saepe consequatur iste recusandae
-            aspernatur molestias sit ex itaque
-          </p>
-          <ul>
-            <li>Price: $$$</li>
-            <li>Includes png, psd, jpg</li>
-          </ul>
-        </div>
-        <div class="Category__placeholder col-xs-12 col-md-5">
-          <q-img src="~assets/images/placeholder_thumb.png" alt="placeholder" />
+    <div else v-for="item in tipos" :key="item._id">
+      <q-separator color="light-gray" />
+      <div class="Comisiones__section" >
+        <div class="Comisiones__Category row q-col-gutter-lg">
+          <div class="Category__description col-xs-12 col-md-7">
+            <h2 class="SectionTitle">{{item.name}}</h2>
+            <p>
+              {{item.description}}
+            </p>
+            <ul>
+              <li>Price: {{item.precioBase}}</li>
+            </ul>
+          </div>
+          <div class="Category__placeholder col-xs-12 col-md-5">
+            <q-img :src="item.img" :alt="item.name" />
+          </div>
         </div>
       </div>
     </div>
@@ -88,6 +50,28 @@
 <script>
 export default {
   name: "ComisionesMain",
+  mounted() {
+    if (this.tipos.length <= 0) {
+      this.$store.dispatch('types/loadTypes')
+        .catch(() => {
+          this.$q.notify({
+            type: 'negative',
+            message: `Couldn't load commissions info. Try again later.`
+          })
+        })
+    }
+  },
+  computed: {
+    loading () {
+      return this.$store.state.types.loading 
+    },
+    error () {
+      return this.$store.state.types.error
+    },
+    tipos () {
+      return this.$store.state.types.tipos
+    }
+  }
 };
 </script>
 
